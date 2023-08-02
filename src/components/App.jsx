a78bace4fb5104c9fb2f6e24c084ba69028140bf
Roman0064 <alee93474@gmail.com>
2023-08-02 22:46:44 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,13 @@ import Filter from './Phonebook/Filter/Filter';
 import css from './App.module.css';
 
 const App = () => {
-  const [ contacts, setContacts ] = useState([
-        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  const [ contacts, setContacts ] = useState(() => JSON.parse(localStorage.getItem('contacts')),
+       [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
         {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
         {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
         {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ]);
   const [ filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const contactsParsed = JSON.parse(localStorage.getItem('contacts'));
-    
-    if(contactsParsed){
-      setContacts(contactsParsed);
-    };
-  }, []);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
